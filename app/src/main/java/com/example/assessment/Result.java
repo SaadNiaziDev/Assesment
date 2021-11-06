2c7af0ce3ac22fa438d4362cdf17023f7a267ca8
Saad Ali Khan <saad.niaxi47@gmail.com>
2021-11-06 23:36:38 +0500

    Result Screen set and functionality updated 1/2

--- app/src/main/java/com/example/assessment/Result.java
+++ app/src/main/java/com/example/assessment/Result.java
@@ -3,12 +3,21 @@ package com.example.assessment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
-
+    Button quitButton, shareButton;
+    TextView attemptedField,correctField,wrongField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        quitButton=(Button)findViewById(R.id.quitButton);
+        shareButton=(Button) findViewById(R.id.shareBtn);
+        attemptedField=(TextView) findViewById(R.id.attemptedInput);
+        wrongField=(TextView) findViewById(R.id.wrongInput);
+        correctField=(TextView) findViewById(R.id.correctInput);
+
     }
 }
