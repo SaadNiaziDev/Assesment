170f6b83290ce49e837893ee34b8c3c8c22cf0ee
Saad Ali Khan <saad.niaxi47@gmail.com>
2021-11-06 21:10:18 +0500

    2nd screen layout set

--- app/src/main/java/com/example/assessment/Questionare.java
+++ app/src/main/java/com/example/assessment/Questionare.java
@@ -3,10 +3,18 @@ package com.example.assessment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.content.Intent;
 
 public class Questionare extends AppCompatActivity {
+    RadioGroup radioGroup;
+    RadioButton r1,r2,r3,r4;
+    String questions[] = {};
+    String anwsers[]={};
+    String options[]={};
+    public static int marks=0,correct=0,wrong=0;
     TextView name_msg,roll_msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,7 @@ public class Questionare extends AppCompatActivity {
         name_msg.setText(str);
         String str2 = intent.getStringExtra("RollNum");
         roll_msg.setText(str2);
+
+
     }
 }
