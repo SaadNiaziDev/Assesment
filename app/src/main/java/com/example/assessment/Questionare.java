253235d582c137afb567d54a3091dfe259747f29
Saad Ali Khan <saad.niaxi47@gmail.com>
2021-11-07 00:06:13 +0500

    Result Screen set and functionality updated 2/2

--- app/src/main/java/com/example/assessment/Questionare.java
+++ app/src/main/java/com/example/assessment/Questionare.java
@@ -3,7 +3,6 @@ package com.example.assessment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,7 +14,7 @@ public class Questionare extends AppCompatActivity {
     TextView questionBox;
     RadioGroup radioGroup;
     RadioButton r1,r2,r3,r4;
-    String questions[] = {"Which method can be defined only once in a program?",
+    String[] questions = {"Which method can be defined only once in a program?",
             "Which of these is not a bitwise operator?",
             "Which keyword is used by method to refer to the object that invoked it?",
             "Which of these keywords is used to define interfaces in Java?",
@@ -26,9 +25,9 @@ public class Questionare extends AppCompatActivity {
             "Which of these method of class String is used to compare two String objects for their equality?",
             "An expression involving byte, int, & literal numbers is promoted to which of these?",
     };
-    String anwsers[]={"main method","<=","this","interface","public","import pkg.*","None of the mentioned","java","equals()","int"
+    String[] anwsers ={"main method","<=","this","interface","public","import pkg.*","None of the mentioned","java","equals()","int"
     };
-    String options[]={"finalize method","main method","static method","private method",
+    String[] options ={"finalize method","main method","static method","private method",
             "&","&=","|=","<=",
             "import","this","catch","abstract",
             "Interface","interface","intf","Intf",
@@ -41,78 +40,73 @@ public class Questionare extends AppCompatActivity {
     };
     public static int marks=0,correct=0,wrong=0;
     TextView name_msg,roll_msg;
-    int counter=0;
+    static int counter=0;
     Button submitBtn,quitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionare);
-        name_msg = (TextView)findViewById(R.id.name_display);
-        roll_msg = (TextView)findViewById(R.id.roll_display);
+        name_msg = findViewById(R.id.name_display);
+        roll_msg = findViewById(R.id.roll_display);
         Intent intent = getIntent();
         String str = intent.getStringExtra("Name");
         name_msg.setText(str);
         String str2 = intent.getStringExtra("RollNum");
         roll_msg.setText(str2);
-
-        questionBox=(TextView)findViewById(R.id.questionBox);
-        submitBtn=(Button)findViewById(R.id.submitBtn);
-        quitBtn=(Button)findViewById(R.id.quitBtn);
+        questionBox=findViewById(R.id.questionBox);
+        submitBtn=findViewById(R.id.submitBtn);
+        quitBtn=findViewById(R.id.quitBtn);
         questionBox.setText(questions[0]);
-        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
-        r1=(RadioButton)findViewById(R.id.radioButton);
-        r2=(RadioButton)findViewById(R.id.radioButton2);
-        r3=(RadioButton)findViewById(R.id.radioButton3);
-        r4=(RadioButton)findViewById(R.id.radioButton4);
+        radioGroup=findViewById(R.id.radioGroup);
+        r1=findViewById(R.id.radioButton);
+        r2=findViewById(R.id.radioButton2);
+        r3=findViewById(R.id.radioButton3);
+        r4=findViewById(R.id.radioButton4);
         r1.setText(options[0]);
         r2.setText(options[1]);
         r3.setText(options[2]);
         r4.setText(options[3]);
 
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        submitBtn.setOnClickListener(v -> {
 
-        if(radioGroup.getCheckedRadioButtonId()==-1){
-            Toast.makeText(getApplicationContext(), "Please select one choice", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    if(radioGroup.getCheckedRadioButtonId()==-1){
+        Toast.makeText(getApplicationContext(), "Please select one choice", Toast.LENGTH_SHORT).show();
+        return;
+    }
 
-        RadioButton userAnwser = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
-        String anwserText = userAnwser.getText().toString();
+    RadioButton userAnwser = findViewById(radioGroup.getCheckedRadioButtonId());
+    String anwserText = userAnwser.getText().toString();
 
-        if(anwserText.equals(anwsers[counter])){
-            correct++;
-        }else {
-            wrong++;
-        }
-        counter++;
+    if(anwserText.equals(anwsers[counter])){
+        correct++;
+    }else {
+        wrong++;
+    }
+    counter++;
 
-        if(counter<questions.length)
-        {
-            questionBox.setText(questions[counter]);
-            r1.setText(options[counter*4]);
-            r2.setText(options[counter*4 +1]);
-            r3.setText(options[counter*4 +2]);
-            r4.setText(options[counter*4 +3]);
-        }
-        else
-        {
-            marks=correct;
-            Intent in = new Intent(getApplicationContext(),Result.class);
-            startActivity(in);
-        }
-        radioGroup.clearCheck();
-        }
+    if(counter<questions.length)
+    {
+        questionBox.setText(questions[counter]);
+        r1.setText(options[counter*4]);
+        r2.setText(options[counter*4 +1]);
+        r3.setText(options[counter*4 +2]);
+        r4.setText(options[counter*4 +3]);
+    }
+    else
+    {
+        marks=correct;
+        Intent in = new Intent(getApplicationContext(),Result.class);
+        startActivity(in);
+    }
+    radioGroup.clearCheck();
     });
 
-        quitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Result.class);
-                startActivity(intent);
-            }
+        quitBtn.setOnClickListener(v -> {
+            Intent intent1 =new Intent(getApplicationContext(),Result.class);
+            intent1.putExtra("Name",name_msg.getText().toString());
+            intent1.putExtra("Roll#",roll_msg.getText().toString());
+            startActivity(intent1);
         });
 }
 }
--- app/src/main/java/com/example/assessment/Result.java
+++ app/src/main/java/com/example/assessment/Result.java
@@ -2,22 +2,68 @@ package com.example.assessment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
+    TextView _name,_rollNum;
     Button quitButton, shareButton;
     TextView attemptedField,correctField,wrongField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Intent GET = getIntent();
+        String text1 = GET.getStringExtra("Name");
+        _name.setText(text1);
+        String text2 = GET.getStringExtra("Roll#");
+        _rollNum.setText(text2);
+
         quitButton=(Button)findViewById(R.id.quitButton);
         shareButton=(Button) findViewById(R.id.shareBtn);
         attemptedField=(TextView) findViewById(R.id.attemptedInput);
         wrongField=(TextView) findViewById(R.id.wrongInput);
         correctField=(TextView) findViewById(R.id.correctInput);
 
+        StringBuffer str = new StringBuffer();
+        str.append(Questionare.counter);
+
+        StringBuffer str2 = new StringBuffer();
+        str2.append(Questionare.correct);
+
+        StringBuffer str3 = new StringBuffer();
+        str3.append(Questionare.wrong);
+
+        attemptedField.setText(str);
+        correctField.setText(str2);
+        wrongField.setText(str3);
+
+        Questionare.correct=0;
+        Questionare.wrong=0;
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Total Score = ");
+                sendIntent.putExtra(Intent.EXTRA_TEXT,correctField.getText().toString());
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+            }
+        });
     }
 }
